1bed7382d17df45b745afe7c5b185e0bd202ef74
Desire <sobouti.desire@gmail.com>
2024-11-30 16:28:02 +0100

    Add files via upload

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+	console.log($('html').attr('lang'));
+	if($('html').attr('lang') == 'es') {
+		window.location.pathname = $(location).attr('hostname') + '/es';
+	} else {
+		window.location.pathname = $(location).attr('hostname') + '/en';		
+	}
+
 	$('.project-item').each(function() {
 		$(this).find('button.arrow').click(function() {
 			$(this).toggleClass('active');
